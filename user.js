fcdb7f796e391c24b9c5dde55fbfcf6265d73011
zarina <akunovazarina1@example.com>
2023-05-22 18:05:36 -0700

    done 3 lesson

---
+++ user.js
@@ -0,0 +1,115 @@
+///8-2=6 
+// 1 number 
+
+var myAge= 13
+ // string
+
+ var myNameIs='Zarina'
+
+ //3 boolean 
+ var isRainy=false
+
+ //4 null
+ var myBrother= null
+
+ //5 undefined
+ var myDog;
+
+ //6 objeck {} = [] array
+ var samsung ={
+    model:"redmi 9 A",
+    color:"black",
+    price: 10*9 ,//900
+    owner:myNameIs  //Zarina 
+ }
+
+ //----------CONDITIONS-----------(if,switch)
+ //готовые функции (alert,prompt,confirm)
+
+ var color="red"
+
+// операторы (=,==,===,!,??,&&,||)
+
+if(color==="red"){
+    console.log('STOP');
+}
+
+else{
+    //false
+    console.log('---h---');
+}
+ 
+
+var color="yellow"
+
+if (color===red) {
+} else if {
+    console.log('STOP...')
+}
+ else if (color===yellow){
+    console.log('WAIT...')
+}
+ else if (color===green){
+    console.log('GO...')
+}
+
+
+var operation='+'
+
+if(operation==='+'){
+    console.log(100+50);
+}else if (operation==='-'){
+    console.log(100-50)
+}
+else if (operation==='*'){
+    console.log(100*50)
+}
+else if (operation==='/'){
+    console.log(100/50)
+}
+
+var temp = 40 
+
+if(temp > 32){
+    console.log('КУУУУУУУУН ЫСЫККЫ...')
+}
+else if(temp > 32 && temp < 40){
+    console.log('КУУУУУУУУН ЫСЫККЫ... АБДАААААН ЫСЫККЫ')
+}
+
+var text = prompt ('атынды жaз')
+ if (text.toLowerCase() === "Zarina"){
+    console.log('yes I am');
+}
+
+var someText = "6"
+
+  switch (someText) {
+    case "Monday":
+    console.log('Понедельник')
+    break;
+    case"6":
+    case 6:
+    console.log('Суббота')
+    break;
+    case "Sunday":
+    console.log('Воскресенье')
+    break;
+    default:
+    break;
+  }
+
+  if(!true){
+    console.log('TRUE...');
+  }else{
+    console.log('FALSE...')
+  }
+  
+  var userDB = {
+    name:'Zarina'
+    email:'AZ@gmail.com'
+    password:'123456789'
+  }
+  
+
+  
